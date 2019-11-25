0c5383555d36b34811dfffb4520017951aa68a43
Kaibo Cai <caikaibo7@gmail.com>
2019-11-25 10:42:46 -0600

    implemented get test metrics

--- config.py
+++ config.py
@@ -7,15 +7,6 @@ Config file for iTestMonitor framework
 class Config():
     def __init__(self):
         self.config = yaml.safe_load(open("config.yml"))
-        print("Please enter project dir & name")
-        projectLocation = str(input())
-        print("Please enter whether project support prometheus (true/false)")
-        supportProme = input()
-        print("Please enter test running method")
-        runningMethod = input()
-        self.config["prometheus"]["projectLocation"] = projectLocation
-        self.config["prometheus"]["support"] = supportProme
-        self.config["prometheus"]["runningMethod"] = runningMethod
 
     
     def get_config(self):
@@ -28,16 +19,13 @@ class Config():
         elif self.config["prometheus"]["runningMethod"] == "java":
             return "java -jar" + self.config["prometheus"]["projectLocation"]
         elif self.config["prometheus"]["runningMethod"] == "bash":
-            print("Please enter bash file location")
-            bashFileLoc = input()
-            return "bash " + bashFileLoc
+            return "bash " + self.config["prometheus"]["bashFileLoc"]
         else: 
-            return "wrong runiing method"
+            return "wrong running method"
         
 
     def get_test_run_metrics(self):
-        # TODO
-        pass
+        return self.config["metrics"]["exporterUrl"]
 
 
 
@@ -49,6 +37,7 @@ def main():
     #print(config)
     runningMethod = c.get_test_run_script()
     print(runningMethod)
+    print(config["metrics"]["exporterUrl"])
 
 
 
